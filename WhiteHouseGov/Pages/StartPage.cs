d1dbdb5b5979fe2b9db370f3893cbfae9a695b01
Björn Göransson <bjorn.a.goransson@gmail.com>
2021-10-18 21:31:57 +0200

    Add startpage contenttype id

--- WhiteHouseGov/Pages/StartPage.cs
+++ WhiteHouseGov/Pages/StartPage.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Cloudy.CMS.ContentTypeSupport;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ScholarlySummaries.Pages
 {
+    [ContentType("4c796987-e198-48fe-9175-4ada770cd298")]
     public class StartPage : Page
     {
         public string HeroHeading { get; set; }
