e78243e4f135575d5d7a5e4a5e986a61a5e0b61b
Björn Göransson <bjorn.a.goransson@gmail.com>
2021-10-19 23:48:50 +0200

    Add basic views, controller, frontend code

---
+++ WhiteHouseGov/Controllers/PageController.cs
@@ -0,0 +1,18 @@
+﻿using WhiteHouseGov.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Cloudy.CMS.ModelBinding;
+
+namespace WhiteHouseGov.Controllers
+{
+    public class PageController : Controller
+    {
+        public ActionResult Index([FromContentRoute] Page page)
+        {
+            return View(page.GetType().Name, page);
+        }
+    }
+}
